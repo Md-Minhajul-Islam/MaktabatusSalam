3d0e26729bb204b3e4c7731f9f52706c395fcdfa
Minhaj <minhajulislammuhammad@gmail.com>
2025-08-02 04:35:52 +0600

    added app/routes/

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../db/config/multer");
 const Book = require("../models/book");
+const LatestNews = require("../models/latest_news");
+const Message = require("../models/message");
+const NewsEvents = require("../models/news_events");
+const NewsEventsPhoto = require("../models/news_events_photo");
+const Notice = require("../models/notice");
+const QuranicVerse = require("../models/quranic_verse");
 const cloudinary = require("../db/config/cloudinary");
 
 
@@ -14,7 +20,6 @@ const cloudinary = require("../db/config/cloudinary");
 router.get("/admin/library", (req, res) => {
   locals = {
     title: "admin/library",
-    success: 0,
   };
   res.render("admin/library", { locals });
 });
@@ -134,8 +139,118 @@ router.post("/admin/library/updatebook", upload.single("book-image-update"), asy
 //-----------------------------------------------------------------------------
 
 
+/*
+  ADMIN 
+  MEDIA
+*/
+
+router.get("/admin/media", (req, res) => {
+  locals = {
+    title: "admin/media",
+  };
+  res.render("admin/media", { locals });
+});
 
 
+
+router.post("/admin/library/addMediaInformation", upload.single("news-events-image-upload"), async (req, res) => {
+
+  locals = {
+    title: "admin/media",
+  };
+  
+  try {
+    if (req.body['latest-news-upload'])
+    {
+      await LatestNews.deleteMany({});
+      const latest_news = new LatestNews({
+        news: req.body['latest-news-upload']
+      });
+      await LatestNews.create(latest_news);
+    }
+
+    if (req.body['quranic-verse-num-upload'] && req.body['quranic-verse-upload']) {
+      
+      await QuranicVerse.deleteMany({ verseNum: req.body['quranic-verse-num-upload'] });
+        
+      const quranic_verse = new QuranicVerse({
+        verseNum: req.body['quranic-verse-num-upload'],
+        verse: req.body['quranic-verse-upload'],
+      });
+      await QuranicVerse.create(quranic_verse);
+    }
+
+    const message = await Message.findOne();
+    if (message)
+    {
+      if (req.body['message1-upload'])
+      {
+        message.message1 = req.body['message1-upload'];
+      }
+      if (req.body['message1-author-upload']) {
+        message.message1Author = req.body['message1-author-upload'];
+      }
+      if (req.body['message2-upload']) {
+        message.message2 = req.body['message2-upload'];
+      }
+      if (req.body['message2-author-upload']) {
+        message.message2Author = req.body['message2-author-upload'];
+      }
+    }
+    else if (
+      req.body["message1-upload"] &&
+      req.body["message1-author-upload"] &&
+      req.body["message2-upload"] &&
+      req.body['message2-author-upload']) {
+      
+      const newMessage = new Message({
+        message1: req.body['message1-upload'],
+        message1Author: req.body['message1-author-upload'],
+        message2: req.body['message2-upload'],
+        message2Author: req.body['message2-author-upload'],
+      });
+      await Message.create(newMessage);
+    }
+
+    if (req.body['news-events-id-upload'] && req.body['news-events-description-upload'])
+    {
+      const newNewsEvents = new NewsEvents({
+        id: req.body['news-events-id-upload'].toUpperCase(),
+        description: req.body['news-events-description-upload'],
+      });
+      await NewsEvents.create(newNewsEvents);
+    }
+
+    if (req.body['news-events-id-upload'] && req.file) {
+      const newNewsEventsPhoto = new NewsEventsPhoto({
+        id: req.body['news-events-id-upload'].toUpperCase(),
+        photoUrl: req.file.path,
+        photoId: req.file.filename,
+      });
+      await NewsEventsPhoto.create(newNewsEventsPhoto);
+    }
+
+    if (req.body['notice-id-upload'] && req.body['notice-description-upload']) {
+      const newNotice = new Notice({
+        id: req.body['notice-id-upload'].toUpperCase(),
+        notice: req.body['notice-description-upload'],
+      });
+      await Notice.create(newNotice);
+    }
+    
+
+
+
+
+
+    res.render('admin/media', { locals });
+  } catch (err)
+  {
+    console.log(err);
+    res.render("admin/media", { locals });
+  }
+
+});
 
 
 
--- app/routes/main.js
+++ app/routes/main.js
@@ -1,50 +1,84 @@
-const express = require('express');
+const express = require("express");
 const Book = require("../models/book");
-
+const LatestNews = require("../models/latest_news");
+const Message = require("../models/message");
+const NewsEvents = require("../models/news_events");
+const NewsEventsPhoto = require("../models/news_events_photo");
+const Notice = require("../models/notice");
+const QuranicVerse = require("../models/quranic_verse");
 
 const router = express.Router();
-
-
 
 /*
     GET
     HOME
 */
-router.get('/', (req, res) => {
-
-    locals = {
-        title: 'Home',
+router.get("/", async (req, res) => {
+  locals = {
+    title: "Home",
+  };
+  let latestNews;
+  let quranicVerse = [];
+  let message;
+  let newsEvents = [];
+  let newsEventsPhoto = [];
+  let notice = [];
+  try {
+    latestNews = await LatestNews.findOne();
+    quranicVerse = await QuranicVerse.find();
+    message = await Message.findOne();
+    const allNewsEvents = await NewsEvents.find().sort({ createdAt: -1 });
+    for (let i = 0; i < allNewsEvents.length && i < 3; i++) {
+      newsEvents.push(allNewsEvents[i]);
     }
-    res.render('index', { locals });
-});
+    for (let i = 0; i < newsEvents.length; i++) {
+      const obj = await NewsEventsPhoto.findOne({ id: newsEvents[i].id });
+      if (obj) newsEventsPhoto.push(obj.photoUrl);
+      else newsEventsPhoto.push("/img/logo.png");
+    }
+    const allNotice = await Notice.find().sort({ createdAt: -1 });
+    for (let i = 0; i < allNotice.length && i < 3; i++) {
+      notice.push(allNotice[i]);
+    }
 
+    res.render("index", {
+      locals,
+      latestNews,
+      quranicVerse,
+      message,
+      newsEvents,
+      newsEventsPhoto,
+      notice,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("index", {
+      locals,
+      latestNews,
+      quranicVerse,
+      message,
+      newsEvents,
+      newsEventsPhoto,
+      notice,
+    });
+  }
+});
 
 /*
     GET
     LIBRARY
 */
-router.get('/library', async (req, res) => {
-    locals = {
-        title: 'Library'
-    }
-    try {
-        const books = await Book.find().sort({id: 1});
-        res.render('library', {locals, books });
-    }
-    catch (err) {
-        console.log(err);
-        res.render('library', { locals, books: []});
-    }
+router.get("/library", async (req, res) => {
+  locals = {
+    title: "Library",
+  };
+  try {
+    const books = await Book.find().sort({ id: 1 });
+    res.render("library", { locals, books });
+  } catch (err) {
+    console.log(err);
+    res.render("library", { locals, books: [] });
+  }
 });
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
