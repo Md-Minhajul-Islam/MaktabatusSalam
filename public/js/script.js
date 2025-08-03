7fe0a3c1fbf1205a742c577d5e69a21b57370be2
Minhaj <minhajulislammuhammad@gmail.com>
2025-08-04 00:24:51 +0600

    updated public/

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,25 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    //quranic Verse
     const verse = document.querySelector('.verse');
     const verseNum = document.querySelector(".verse-num");
 
-    if (!verse || !verseNum || !quranicVerse || quranicVerse.length === 0)
-        return;
-
-    function showVerse() {
+    if (verse && verseNum && typeof quranicVerse !== "undefined" && quranicVerse.length > 0) {
+      function showVerse() {
         const index = Math.floor(Math.random() * quranicVerse.length);
         verse.innerText = quranicVerse[index].verse;
         verseNum.innerText = quranicVerse[index].verseNum;
+      }
+      showVerse();
+      setInterval(showVerse, 10000);
     }
-    showVerse();
-    setInterval(showVerse, 10000);
+
+    const administrationButton = document.querySelector('.administration');
+    const administrationPanel = document.querySelector('.administration-panel');
+    if (administrationButton && administrationPanel) {
+        administrationButton.addEventListener("click", () => {
+        administrationPanel.style.visibility = administrationPanel.style.visibility === "hidden"
+            ? "visible" : "hidden";
+      });
+    }
 });
+
